986e7d88fe1bc743f6affb14d576f9ecd218e389
herbertug <herbertoketayot@gmail.com>
2024-08-29 17:53:10 +0300

    Single task component view

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,12 +1,38 @@
 import { Component, Input } from '@angular/core';
+import { SingleTaskComponent } from "./single-task/single-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './task.component.html',
-  styleUrl: './task.component.css'
+  styleUrl: './task.component.css',
+  imports: [SingleTaskComponent]
 })
 export class TaskComponent {
   @Input() name?:string;
-
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary:
+        'Learn all the basic and advanced features of Angular & how to apply them.',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'Build a first prototype of the online shop website',
+      dueDate: '2024-05-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue template',
+      summary:
+        'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
+  ]
 }
